0efb2d7b156a2bec122ec780a91ea81d9aa430c4
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-08-03 16:22:47 +0100

    commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp with specific requirements
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
